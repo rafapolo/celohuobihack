44e94f25c02b5feff28a8f0c1a98862e81b8a731
extrapo <x@extrapolo.com>
2022-06-24 16:25:08 +0100

    fix toWei(amount.toString())

--- veraefiko/dapp/src/dapp.js
+++ veraefiko/dapp/src/dapp.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  
+  const CONTRACT = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+  
   if (typeof window.ethereum !== 'undefined') {
       let currentAccount = null;
       if (window.ethereum.isMetaMask) {
@@ -16,18 +19,20 @@ $(document).ready(function() {
             }
         }
         return false;
-      });
-      
+      });      
+            
       // read metadata from ipns
       function read_metadata(){
-        // todo: get address from contract
+        // wip: get address from contract
+        // window.contract.methods.ipns_path()
         let ipns_oracle = "QmR5RCD47Y4DiUcDso4hpt2fmbJ87gQyYUBMFD4ojBU5jY"
+        
+        console.log("fetching ipfs:// metadata ...")
         $.getJSON("https://ipfs.io/ipfs/"+ipns_oracle, function(data){
-          console.log("fetching ipfs:// metadata ...")
         }).done(function(result) {
           console.log(result);
           $.each( result['data'] , function( tkn, val ) {
-            $("#tokens").append( "<span class='token' id='" + tkn + "'>" + tkn.toUpperCase() + " - cR$" + parseFloat(val['valor']).toFixed(2) + "</span>" );
+            $("#tokens").append( "<span class='token' id='" + tkn + "'><b>" + tkn.toUpperCase() + "</b> cR$" + parseFloat(val['valor']).toFixed(2) + "</span>" );
           });
         }).fail(function(jqXHR, status, error){
           console.log(error);
@@ -39,7 +44,7 @@ $(document).ready(function() {
           console.log("contract abi:");
           console.log(data);
           const ABI = data;
-          window.contract = new window.web3.eth.Contract(ABI, "0xd9145CCE52D386f254917e481eB44e9943F39138"); 
+          window.contract = new window.web3.eth.Contract(ABI, CONTRACT); 
         });
       } 
             
@@ -54,6 +59,7 @@ $(document).ready(function() {
           handleAccountsBalance(currentAccount);
           $(".showAccount").text(currentAccount);
           read_metadata();
+          loadContract();
   			}
   		}
           
@@ -66,6 +72,10 @@ $(document).ready(function() {
           $(".showBalance").text(window.web3.utils.hexToNumberString(result) + " cREAL");
           $(".auth").hide();
           $(".wallet").show();
+          // topup cREAL address!
+          $('.topup').on('click', function(){
+            window.open("https://moedax.digital/topup="+account);
+          });          
         }).catch(err=>console.log(err))
       }
       
@@ -76,7 +86,7 @@ $(document).ready(function() {
           params: {
             chainId: '0xa4ec',
             chainName: 'Celo',
-            nativeCurrency: { name: 'Celo', symbol: 'CELO', decimals: 18 }, 
+            nativeCurrency: { name: 'cREAL', symbol: 'cREAL', decimals: 18 }, 
             rpcUrls: ['https://forno.celo.org'],
             blockExplorerUrls: ['https://explorer.celo.org/'],
             iconUrls: ['future']
@@ -92,16 +102,17 @@ $(document).ready(function() {
       // buy token
       $(document).on("click", '.token', function(){
         token = $(this).attr("id");
-        // todo: form
-        // (address, token, chain, amount)
-        amount = 10.5;
+        // todo: html form
+        // define (address, token, chain, amount)
+        amount = 10.5; // wip
+        // important: agent_wallet should be in the metadata file 
         agent_wallet = "0x12c473b6F86639738AFCcc6f26914619cFf669a5";
         window.contract.methods.addOrder(agent_wallet,"CELO","CELO",amount);
         alert("Nice! You have to send a cREAL deposit of " +amount+ " so we you proceeed with your exchange.");
         window.web3.eth.sendTransaction({
            from: currentAccount,
            to: agent_wallet,
-           value: amount,
+           value: window.web3.utils.toWei(amount.toString()),
         }, function (err, transactionHash) {
           console.error(err);        
         });
